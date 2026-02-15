3f53140d65d64ae9357e921cdff9bf692915f79a
Roman Yurchenko <roman.yurchenko.3@gmail.com>
2026-02-15 15:25:32 +0300

    little style update

--- quests.py
+++ quests.py
@@ -2,10 +2,14 @@ nikolay2 = {
     "id": "nikolay2",
     "title": "Расследование убийства Николая II",
     "description": "Исторический квест о последних днях семьи Романовых.",
+    "card_image": "images/nikolay2_0.jpg",
     "questions": [
         {
             "context": "После отречения от престола Николай II и его семья были арестованы.",
-            "media": None,
+            "media": {
+                "type": "image",
+                "file": "nikolay2_1.jpg"
+            },
             "question": "Где содержалась семья Романовых перед казнью?",
             "answers": [
                 {
@@ -34,3 +38,42 @@ nikolay2 = {
 ALL_QUESTS = {
     nikolay2["id"]: nikolay2
 }
+
+
+
+# шаблон
+
+# quest = {
+#     "id": "quest",
+#     "title": "quest title",
+#     "description": "quest description",
+#     "card_image": "images/quest.jpg",
+#     "questions": [
+#         {
+#             "context": "context first question",
+#             "media": {
+#                 "type": "image / video / None",
+#                 "file": "quest_question_1.jpg"
+#             },
+#             "question": "question",
+#             "answers": [
+#                 {
+#                     "text": "first answer",
+#                     "correct": False / True,
+#                     "explanation": "answer explanation"
+#                 },
+#                 {
+#                     "text": "second answer",
+#                     "correct": False / True,
+#                     "explanation": "answer explanation"
+#                 },
+#                 {
+#                     "text": "third answer",
+#                     "correct": False / True,
+#                     "explanation": "answer explanation"
+#                 }
+#             ]
+#         }
+#     ],
+#     "result_text": "quest result text",
+# }
