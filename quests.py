ef6d1a8277d1687f0f7fb20da74fd2b720220af9
Roman Yurchenko <roman.yurchenko.3@gmail.com>
2026-02-15 13:46:24 +0300

    test update, deleted quest

--- quests.py
+++ quests.py
@@ -29,45 +29,8 @@ nikolay2 = {
     "result_text": "Вы проследили цепочку событий, приведших к трагической развязке."
 }
 
-ekaterina2 = {
-    "id": "ekaterina2",
-    "title": "Екатерина Великая",
-    "description": "Узнайте великую Екатерину II поближе.",
-    "questions": [
-        {
-            "context": "У Екатерины был муж.",
-            "media": None,
-            "question": "Кто был мужем Екатерины?",
-            "answers": [
-                {
-                    "text": "Петр III",
-                    "correct": True,
-                    "explanation": "Он действительно был её супругом, но относились они друг к другу очень холодно и даже агрессивно"
-                },
-                {
-                    "text": "Петр I",
-                    "correct": False,
-                    "explanation": "Ты додик?"
-                },
-                {
-                    "text": "Николай",
-                    "correct": False,
-                    "explanation": "Ты додик?"
-                },
-                {
-                    "text": "Иван грозный",
-                    "correct": False,
-                    "explanation": "Совсем олень?"
-                }
-            ]
-        }
-    ],
-    "result_text": "Ну ты и додик.",
-}
-
 
 
 ALL_QUESTS = {
-    nikolay2["id"]: nikolay2,
-    ekaterina2["id"]: ekaterina2
+    nikolay2["id"]: nikolay2
 }
